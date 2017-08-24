9c626423e694929d86fee4dec5f5b6c9c046bb99
Moacir Braga <contato@moacirbrg.org>
2017-08-24 00:15:02 -0300

    SmartFormat sample

---
+++ SmartFormatSample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SmartFormat;
+
+namespace SmartFormatSample
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var str = Smart.Format("My name is {name}, I am {age} years old", new { name = "Moacir", age = 99 });
+      Console.WriteLine(str);
+    }
+  }
+}
