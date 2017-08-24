4797e5c8e3dc1e389c06c7912db02b7b1d0588b6
Moacir Braga <contato@moacirbrg.org>
2017-08-24 00:25:48 -0300

    Markdig sample

---
+++ MarkdigSample/Program.cs
@@ -0,0 +1,17 @@
+﻿using Markdig;
+using MarkdigSample.Properties;
+using System;
+
+namespace MarkdigSample
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var resource = System.Text.Encoding.Default.GetString(Resources.readme);
+      var pipeline = new MarkdownPipelineBuilder().UseGridTables().UseEmphasisExtras().UseFootnotes().UseMediaLinks().Build();
+      var result = Markdown.ToHtml(resource, pipeline);
+      Console.WriteLine(result);
+    }
+  }
+}
---
+++ MarkdigSample/readme.md
@@ -0,0 +1,17 @@
+First line
+
+# Title
+
+My name is *Moacir*
+
+```c
+int main()
+{
+  int a = 0;
+}
+```
+
++-+-+
+|A|B|
++-+-+
+|a|a|
