62a6c45e879dd2959f71753b8ef2b2eb9f185dac
Moacir Braga <contato@moacirbrg.org>
2017-08-24 00:41:47 -0300

    Newtonsoft sample

---
+++ NewtonsoftSample/Program.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using NewtonsoftSample.Properties;
+using System;
+
+namespace NewtonsoftSample
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var fileContent = System.Text.Encoding.UTF8.GetString(Resources.user);
+      var user = JsonConvert.DeserializeObject<User>(fileContent);
+      Console.WriteLine(string.Format("User: {0}, Age: {1}", user.Name, user.Age));
+    }
+  }
+}
---
+++ NewtonsoftSample/User.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace NewtonsoftSample
+{
+  class User
+  {
+    [JsonProperty("name2")]
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+  }
+}
