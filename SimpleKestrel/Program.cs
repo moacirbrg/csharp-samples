5c9b6218183285ee0d701f61802c42e56adaaecf
Moacir Braga <contato@moacirbrg.org>
2017-08-23 23:55:42 -0300

    Simple Kestrel

---
+++ SimpleKestrel/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+
+namespace SimpleKestrel
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      new WebHostBuilder()
+        .UseKestrel(options => {
+          options.Listen(IPAddress.Any, 8080);
+        })
+        .UseLibuv()
+        .UseStartup<Startup>()
+        .Build()
+        .Run();
+    }
+
+    class Startup
+    {
+      public void Configure(IApplicationBuilder app)
+      {
+        app.Run(ctx => {
+          var con = ctx.Connection;
+          var res = "Hello World!";
+          ctx.Response.ContentLength = res.Length;
+          ctx.Response.ContentType = "text/plain";
+          return ctx.Response.WriteAsync(res);
+        });
+      }
+    }
+  }
+}
